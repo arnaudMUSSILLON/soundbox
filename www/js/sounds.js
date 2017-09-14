ea19300085d7229809365c50182a5f76c7651c35
Clément PAQUIS <clementpaquis@gmail.com>
2017-09-14 17:22:19 +0200

    ihm + background color changes

--- www/js/sounds.js
+++ www/js/sounds.js
@@ -1,10 +1,18 @@
 sounds = [
-  {id:1, name:"ah", path:"sounds/ha.mp3"},
-  {id:2, name:"ah Remix1", path:"sounds/ahremix1.mp3"},
-  {id:3, name:"Filme mes pieds", path:"sounds/filmemespieds.mp3"},
-  {id:4, name:"Pas le temps de niaiser", path:"sounds/pasletempsdeniaiser.mp3"},
-  {id:5, name:"Votre épée", path:"sounds/votreepee.mp3"},
+  {id:1, name:"AH !", path:"sounds/ha.mp3"},
+  {id:2, name:"AH !", path:"sounds/ahremix1.mp3"},
+  {id:3, name:"FILME MES PIEDS", path:"sounds/filmemespieds.mp3"},
+  {id:4, name:"PAS LE TEMPS", path:"sounds/pasletempsdeniaiser.mp3"},
+  {id:5, name:"VOTRE ÉPÉE", path:"sounds/votreepee.mp3"},
 ]
+
+/*sounds = [
+    {id:1, name:"AH !", path:"sounds/ha.mp3"},
+    {id:2, name:"AH ! Remix #1", path:"sounds/ahremix1.mp3"},
+    {id:3, name:"FILME MES PIEDS", path:"sounds/filmemespieds.mp3"},
+    {id:4, name:"PAS LE TEMPS DE NIAISER", path:"sounds/pasletempsdeniaiser.mp3"},
+    {id:5, name:"ELLE EST GROSSE VOTRE ÉPÉE", path:"sounds/votreepee.mp3"},
+]*/
 
 $.each(sounds, function(index, value){
   $("#sound-container").append('<div class="box">'
@@ -15,5 +23,13 @@ $.each(sounds, function(index, value){
 });
 
 $(".box").on("click", function(){
-  $(this).find('audio')[0].play();
+  var sound = $(this).find('audio')[0];
+  sound.play();
+  $(this).css('background-color','yellow');
+  var that = $(this);
+  sound.addEventListener('ended',function(that){
+    alert('ca marche');
+    $(that).css("background-color","red")
+  });
+
 })
