e69b94d6f09ae88a6ed380baa58f2ade2c1f42c1
Arnaud MUSSILLON <arnaud.mussillon@orange.fr>
2017-09-15 13:59:08 +0200

    search feature

--- www/js/search.js
+++ www/js/search.js
@@ -1,6 +1,19 @@
 $("#search").on('click', function(){
+  //display search input
   var header = $(this).parent();
   $(this).remove();
   header.append('<div class="search-box wow animated bounceInRight"><div class="search-icon"><i class="fa fa-search"></i>'+
-    '</div><input type="text" class="search-input" placeholder="Search ..."></div>');
+    '</div><input type="text" id="search-input" placeholder="Search ..."></div>');
+
+    //search function
+    $('#search-input').keyup(function(){
+      for(var i=0; i<sounds.length;i++){
+        //display sound if it matchs to the search
+        if(sounds[i].name.toUpperCase().indexOf($('#search-input').val().toUpperCase()) > -1){
+          $('#'+i).parent()[0].style.display = "";
+        }else {
+          $('#'+i).parent()[0].style.display = "none";
+        }
+      }
+    });
 });
--- www/js/sounds.js
+++ www/js/sounds.js
@@ -1,17 +1,15 @@
 sounds = [
-  {id:1, name:"ah", path:"sounds/ha.mp3"},
-  {id:2, name:"ah Remix1", path:"sounds/ahremix1.mp3"},
-  {id:3, name:"Filme mes pieds", path:"sounds/filmemespieds.mp3"},
-  {id:4, name:"Pas le temps de niaiser", path:"sounds/pasletempsdeniaiser.mp3"},
-  {id:5, name:"Votre épée", path:"sounds/votreepee.mp3"},
+  {id:0, name:"ah", path:"sounds/ha.mp3"},
+  {id:1, name:"ah Remix1", path:"sounds/ahremix1.mp3"},
+  {id:2, name:"Filme mes pieds", path:"sounds/filmemespieds.mp3"},
+  {id:3, name:"Pas le temps de niaiser", path:"sounds/pasletempsdeniaiser.mp3"},
+  {id:4, name:"Votre épée", path:"sounds/votreepee.mp3"},
 ]
 
 $.each(sounds, function(index, value){
-  $("#sound-container").append('<div class="box">'
-    +value.name
-    +'<audio id='+value.id+' src='
-    +value.path
-    +'/></audio></div>');
+  $("#sound-container").append('<div class="box">'+value.name+'<audio id='
+    +value.id+' src='
+    +value.path+'/></audio></div>');
 });
 
 $(".box").on("click", function(){
